4a5fbcbb190c2587fc2cc34f510150c2d78290be
Heather Lanigan <heather.lanigan@canonical.com>
2022-09-07 13:47:42 -0400

    Add tests for Status, StatusHistory, FindTools and SetModelAgentVersion.

--- api/client/client/client_test.go
+++ api/client/client/client_test.go
@@ -4,23 +4,27 @@
 package client_test
 
 import (
-	"bytes"
+	"context"
+	"fmt"
 	"io/ioutil"
-	"net"
 	"net/http"
-	"net/url"
-	"path"
+	"strings"
 
 	"github.com/golang/mock/gomock"
 	"github.com/juju/errors"
+	"github.com/juju/names/v4"
 	jc "github.com/juju/testing/checkers"
 	"github.com/juju/version/v2"
 	gc "gopkg.in/check.v1"
+	"gopkg.in/httprequest.v1"
 
+	"github.com/juju/juju/api"
 	basemocks "github.com/juju/juju/api/base/mocks"
 	"github.com/juju/juju/api/client/client"
-	jujunames "github.com/juju/juju/juju/names"
-	coretesting "github.com/juju/juju/testing"
+	"github.com/juju/juju/api/client/client/mocks"
+	corestatus "github.com/juju/juju/core/status"
+	"github.com/juju/juju/rpc/params"
+	coretools "github.com/juju/juju/tools"
 )
 
 type clientSuite struct {
@@ -28,65 +32,114 @@ type clientSuite struct {
 
 var _ = gc.Suite(&clientSuite{})
 
-func (s *clientSuite) TestUploadToolsOtherModel(c *gc.C) {
+func (s *clientSuite) TestStatus(c *gc.C) {
 	ctrl := gomock.NewController(c)
 	defer ctrl.Finish()
 
 	mockFacadeCaller := basemocks.NewMockFacadeCaller(ctrl)
+
+	pattern := []string{"ubuntu/0"}
+	args := params.StatusParams{Patterns: pattern}
+	result := new(params.FullStatus)
+	hostname := "juju-testme"
+	results := params.FullStatus{
+		Model:               params.ModelStatusInfo{},
+		Machines:            map[string]params.MachineStatus{"0": {Hostname: hostname}},
+		Applications:        nil,
+		RemoteApplications:  nil,
+		Offers:              nil,
+		Relations:           nil,
+		ControllerTimestamp: nil,
+		Branches:            nil,
+	}
+	mockFacadeCaller.EXPECT().FacadeCall("FullStatus", args, result).SetArg(2, results).Return(nil)
+
+	testClient := client.NewClientFromCaller(mockFacadeCaller)
+	obtainedStatus, err := testClient.Status(pattern)
+	c.Assert(err, jc.ErrorIsNil) // Confirms that the correct facade was called
+	c.Assert(obtainedStatus.Model.Type, gc.Equals, "iaas")
+	m, ok := obtainedStatus.Machines["0"]
+	c.Assert(ok, jc.IsTrue)
+	c.Assert(m.Hostname, gc.Equals, hostname)
+}
+
+func (s *clientSuite) TestStatusHistory(c *gc.C) {
+	ctrl := gomock.NewController(c)
+	defer ctrl.Finish()
+
+	mockFacadeCaller := basemocks.NewMockFacadeCaller(ctrl)
+
+	tag := names.NewMachineTag("0")
+	result := new(params.StatusHistoryResults)
+	results := params.StatusHistoryResults{
+		Results: []params.StatusHistoryResult{{
+			History: params.History{
+				Statuses: []params.DetailedStatus{
+					{
+						Kind: corestatus.KindMachine.String(),
+					},
+				},
+			},
+			Error: nil,
+		},
+		}}
+	mockFacadeCaller.EXPECT().FacadeCall("StatusHistory", gomock.AssignableToTypeOf(params.StatusHistoryRequests{}), result).SetArg(2, results).DoAndReturn(
+		func(request string, args, response interface{}) error {
+			p, ok := args.(params.StatusHistoryRequests)
+			c.Assert(ok, jc.IsTrue)
+			c.Assert(p.Requests, gc.HasLen, 1)
+			c.Assert(p.Requests[0].Kind, gc.Equals, corestatus.KindMachine.String())
+			c.Assert(p.Requests[0].Filter.Size, gc.Equals, 10)
+
+			return nil
+		})
+
 	testClient := client.NewClientFromCaller(mockFacadeCaller)
 
-	newVersion := version.MustParseBinary("5.4.3-ubuntu-amd64")
-	var called bool
-
-	// build fake tools
-	expectedTools, _ := coretesting.TarGz(
-		coretesting.NewTarFile(jujunames.Jujud, 0777, "jujud contents "+newVersion.String()))
-
-	// UploadTools does not use the facades, so instead of patching the
-	// facade call, we set up a fake endpoint to test.
-	defer func() {
-		_ = fakeAPIEndpoint(c, testClient, modelEndpoint(c, "tools"), "POST",
-			func(w http.ResponseWriter, r *http.Request) {
-				called = true
-
-				c.Assert(r.URL.Query(), gc.DeepEquals, url.Values{
-					"binaryVersion": []string{"5.4.3-ubuntu-amd64"},
-					"series":        []string{""},
-				})
-				defer func() { _ = r.Body.Close() }()
-				obtainedTools, err := ioutil.ReadAll(r.Body)
-				c.Assert(err, jc.ErrorIsNil)
-				c.Assert(obtainedTools, gc.DeepEquals, expectedTools)
-			},
-		).Close()
-	}()
-
-	// We don't test the error or tools results as we only wish to assert that
-	// the API client POSTs the tools archive to the correct endpoint.
-	_, _ = testClient.UploadTools(bytes.NewReader(expectedTools), newVersion)
-	c.Assert(called, jc.IsTrue)
+	obtainedHistory, err := testClient.StatusHistory(corestatus.KindMachine, tag, corestatus.StatusHistoryFilter{Size: 10})
+	c.Assert(err, jc.ErrorIsNil) // Confirms that the correct facade was called
+	c.Assert(obtainedHistory, gc.HasLen, 1)
 }
 
-func fakeAPIEndpoint(c *gc.C, cl *client.Client, address, method string, handle func(http.ResponseWriter, *http.Request)) net.Listener {
-	lis, err := net.Listen("tcp", "127.0.0.1:0")
+func (s *clientSuite) TestWatchAll(c *gc.C) {
+	ctrl := gomock.NewController(c)
+	defer ctrl.Finish()
+
+	mockFacadeCaller := basemocks.NewMockFacadeCaller(ctrl)
+	mockConnection := mocks.NewMockConnection(ctrl)
+
+	result := new(params.AllWatcherId)
+	results := params.AllWatcherId{AllWatcherId: "watcher"}
+	mockFacadeCaller.EXPECT().FacadeCall("WatchAll", nil, result).SetArg(2, results).Return(nil)
+
+	testClient := client.NewClientFromCallerAndConnection(mockFacadeCaller, mockConnection)
+	w, err := testClient.WatchAll()
 	c.Assert(err, jc.ErrorIsNil)
-
-	mux := http.NewServeMux()
-	mux.HandleFunc(address, func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == method {
-			handle(w, r)
-		}
-	})
-	go func() {
-		http.Serve(lis, mux)
-	}()
-	client.SetServerAddress(cl, "http", lis.Addr().String())
-	return lis
+	//c.Assert(w, jc.Satisfies, *api.AllWatcher)
+	//	var logWriter raftleasestore.Logger
+	//	c.Assert(args[1], gc.Implements, &logWriter)
+	var allWatcher *api.AllWatch
+	c.Assert(w, gc.Implements, allWatcher)
 }
 
-// modelEndpoint returns "/model/<model-uuid>/<destination>"
-func modelEndpoint(c *gc.C, destination string) string {
-	return path.Join("/model", coretesting.ModelTag.String(), destination)
+func (s *clientSuite) TestSetModelAgentVersion(c *gc.C) {
+	ctrl := gomock.NewController(c)
+	defer ctrl.Finish()
+
+	mockFacadeCaller := basemocks.NewMockFacadeCaller(ctrl)
+
+	ver := version.MustParse("2.9.100")
+	args := params.SetModelAgentVersion{
+		Version:             ver,
+		AgentStream:         "test-stream",
+		IgnoreAgentVersions: true,
+	}
+
+	mockFacadeCaller.EXPECT().FacadeCall("SetModelAgentVersion", args, nil).Return(nil)
+
+	testClient := client.NewClientFromCaller(mockFacadeCaller)
+	err := testClient.SetModelAgentVersion(ver, "test-stream", true)
+	c.Assert(err, jc.ErrorIsNil) // Confirms that the correct facade was called
 }
 
 func (s *clientSuite) TestAbortCurrentUpgrade(c *gc.C) {
@@ -100,4 +153,76 @@ func (s *clientSuite) TestAbortCurrentUpgrade(c *gc.C) {
 	testClient := client.NewClientFromCaller(mockFacadeCaller)
 	err := testClient.AbortCurrentUpgrade()
 	c.Assert(err, gc.Equals, someErr) // Confirms that the correct facade was called
+}
+
+func (s *clientSuite) TestFindTools(c *gc.C) {
+	ctrl := gomock.NewController(c)
+	defer ctrl.Finish()
+
+	mockFacadeCaller := basemocks.NewMockFacadeCaller(ctrl)
+
+	ver := version.MustParseBinary("2.9.100-ubuntu-amd64")
+	args := params.FindToolsParams{
+		MajorVersion: ver.Major,
+		MinorVersion: ver.Minor,
+		Arch:         ver.Arch,
+		OSType:       "ubuntu",
+		AgentStream:  "test-stream",
+	}
+	list := coretools.List{
+		&coretools.Tools{Version: ver},
+	}
+	results := params.FindToolsResult{
+		List: list,
+	}
+	result := new(params.FindToolsResult)
+	mockFacadeCaller.EXPECT().FacadeCall("FindTools", args, result).SetArg(2, results).Return(nil)
+
+	testClient := client.NewClientFromCaller(mockFacadeCaller)
+	obtained, err := testClient.FindTools(2, 9, "ubuntu", "amd64", "test-stream")
+	c.Assert(err, jc.ErrorIsNil) // Confirms that the correct facade was called
+	c.Assert(obtained.List, gc.DeepEquals, list)
+}
+
+func (s *clientSuite) TestUploadTools(c *gc.C) {
+	ctrl := gomock.NewController(c)
+	defer ctrl.Finish()
+
+	mockDoer := mocks.NewMockDoer(ctrl)
+	mockAPICallCloser := basemocks.NewMockAPICallCloser(ctrl)
+	mockFacadeCaller := basemocks.NewMockFacadeCaller(ctrl)
+	mockConnection := mocks.NewMockConnection(ctrl)
+
+	mockConnection.EXPECT().HTTPClient().Return(&httprequest.Client{Doer: mockDoer}, nil)
+	mockFacadeCaller.EXPECT().RawAPICaller().Return(mockAPICallCloser)
+	mockAPICallCloser.EXPECT().Context().Return(context.TODO())
+
+	ver := version.MustParseBinary("2.9.100-ubuntu-amd64")
+
+	req, err := http.NewRequest(
+		"POST",
+		fmt.Sprintf("/tools?binaryVersion=%s&series=", ver),
+		nil,
+	)
+	c.Assert(err, jc.ErrorIsNil)
+	req.Header.Set("Content-Type", "application/x-tar-gz")
+	req = req.WithContext(context.TODO())
+
+	resp := &http.Response{
+		Request:    req,
+		StatusCode: http.StatusCreated,
+		Header:     http.Header{},
+		Body:       ioutil.NopCloser(strings.NewReader(fmt.Sprintf(`{"tools": [{"version": "%s"}]}`, ver))),
+	}
+	resp.Header.Set("Content-Type", "application/json")
+	mockDoer.EXPECT().Do(req).Return(resp, nil)
+
+	testClient := client.NewClientFromCallerAndConnection(mockFacadeCaller, mockConnection)
+	result, err := testClient.UploadTools(
+		nil, ver,
+	)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Assert(result, gc.DeepEquals, coretools.List{
+		{Version: ver},
+	})
 }
--- api/client/client/package_test.go
+++ api/client/client/package_test.go
@@ -1,14 +1,33 @@
 // Copyright 2022 Canonical Ltd.
 // Licensed under the AGPLv3, see LICENCE file for details.
 
-package client_test
+package client
 
 import (
 	"testing"
 
 	gc "gopkg.in/check.v1"
+
+	"github.com/juju/juju/api"
+	"github.com/juju/juju/api/base"
 )
+
+//go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/connection_mock.go github.com/juju/juju/api Connection
+//go:generate go run github.com/golang/mock/mockgen -package mocks -destination mocks/httprequest_mock.go gopkg.in/httprequest.v1 Doer
 
 func TestAll(t *testing.T) {
 	gc.TestingT(t)
+}
+
+func NewClientFromCaller(caller base.FacadeCaller) *Client {
+	return &Client{
+		facade: caller,
+	}
+}
+
+func NewClientFromCallerAndConnection(caller base.FacadeCaller, conn api.Connection) *Client {
+	return &Client{
+		facade: caller,
+		conn:   conn,
+	}
 }
