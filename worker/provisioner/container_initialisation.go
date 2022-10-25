fec439e4eedffb2630b808f7f6a586b1422060e8
Heather Lanigan <heather.lanigan@canonical.com>
2022-10-25 14:01:25 -0400

    Remove unused code, add timer to lock aquire

--- worker/provisioner/container_initialisation.go
+++ worker/provisioner/container_initialisation.go
@@ -5,7 +5,7 @@ package provisioner
 
 import (
 	"fmt"
-	"sync/atomic"
+	"time"
 
 	"github.com/juju/errors"
 	"github.com/juju/worker/v3"
@@ -16,12 +16,9 @@ import (
 	"github.com/juju/juju/container"
 	"github.com/juju/juju/container/kvm"
 	"github.com/juju/juju/container/lxd"
-	corecontainer "github.com/juju/juju/core/container"
 	"github.com/juju/juju/core/instance"
 	"github.com/juju/juju/core/machinelock"
 	"github.com/juju/juju/core/network"
-	"github.com/juju/juju/core/watcher"
-	"github.com/juju/juju/environs"
 	"github.com/juju/juju/environs/config"
 	"github.com/juju/juju/rpc/params"
 	workercommon "github.com/juju/juju/worker/common"
@@ -33,7 +30,6 @@ import (
 type ContainerSetup struct {
 	// abort used for init container dependencies
 	abort         <-chan struct{}
-	runner        *worker.Runner
 	logger        Logger
 	containerType instance.ContainerType
 	provisioner   *apiprovisioner.State
@@ -41,11 +37,6 @@ type ContainerSetup struct {
 	config        agent.Config
 	machineLock   machinelock.Lock
 
-	// Save the workerName so the worker thread can be stopped.
-	workerName string
-	// setupDone[containerType] is non zero if the container setup has been
-	// invoked for that container type.
-	setupDone map[instance.ContainerType]*int32
 	// The number of provisioners started. Once all necessary provisioners have
 	// been started, the container watcher can be stopped.
 	numberProvisioners int32
@@ -58,7 +49,6 @@ type ContainerSetupParams struct {
 	Abort         <-chan struct{}
 	Runner        *worker.Runner
 	Logger        Logger
-	WorkerName    string
 	ContainerType instance.ContainerType
 	Machine       apiprovisioner.MachineProvisioner
 	Provisioner   *apiprovisioner.State
@@ -67,25 +57,11 @@ type ContainerSetupParams struct {
 	CredentialAPI workercommon.CredentialAPI
 }
 
-// NewContainerSetupHandler returns a StringsWatchHandler which is notified
-// when containers are created on the given machine.
+// NewContainerSetup returns a ContainerSetup to start the container
+// provisioner workers.
 func NewContainerSetup(params ContainerSetupParams) *ContainerSetup {
 	return &ContainerSetup{
-		runner:        params.Runner,
-		logger:        params.Logger,
-		machine:       params.Machine,
-		containerType: params.ContainerType,
-		provisioner:   params.Provisioner,
-		config:        params.Config,
-		workerName:    params.WorkerName,
-		machineLock:   params.MachineLock,
-		credentialAPI: params.CredentialAPI,
-		getNetConfig:  common.GetObservedNetworkConfig,
-	}
-}
-
-func NewContainerSetupHandler(params ContainerSetupParams) watcher.StringsHandler {
-	return &ContainerSetup{
+		abort:         params.Abort,
 		logger:        params.Logger,
 		machine:       params.Machine,
 		containerType: params.ContainerType,
@@ -96,124 +72,6 @@ func NewContainerSetupHandler(params ContainerSetupParams) watcher.StringsHandle
 		getNetConfig:  common.GetObservedNetworkConfig,
 	}
 }
-
-// SetUp is defined on the StringsWatchHandler interface.
-func (cs *ContainerSetup) SetUp() (watcher watcher.StringsWatcher, err error) {
-	// Set up the semaphores for each container type.
-	cs.setupDone = make(map[instance.ContainerType]*int32, len(instance.ContainerTypes))
-	for _, containerType := range instance.ContainerTypes {
-		zero := int32(0)
-		cs.setupDone[containerType] = &zero
-	}
-	// Listen to all container lifecycle events on our machine.
-	if watcher, err = cs.machine.WatchAllContainers(); err != nil {
-		return nil, err
-	}
-	return watcher, nil
-}
-
-// Handle is called whenever containers change on the machine being watched.
-// Machines start out with no containers so the first time Handle is called,
-// it will be because a container has been added.
-func (cs *ContainerSetup) Handle(abort <-chan struct{}, containerIds []string) (resultError error) {
-	// Consume the initial watcher event.
-	if len(containerIds) == 0 {
-		return nil
-	}
-
-	cs.logger.Infof("initial container setup with ids: %v", containerIds)
-	for _, id := range containerIds {
-		containerType := corecontainer.ContainerTypeFromId(id)
-		// If this container type has been dealt with, do nothing.
-		if atomic.LoadInt32(cs.setupDone[containerType]) != 0 {
-			continue
-		}
-		//if err := cs.initialiseAndStartProvisioner(abort, containerType); err != nil {
-		//	cs.logger.Errorf("starting container provisioner for %v: %v", containerType, err)
-		//	// Just because dealing with one type of container fails, we won't
-		//	// exit the entire function because we still want to try and start
-		//	// other container types. So we take note of and return the first
-		//	// such error.
-		//	if resultError == nil {
-		//		resultError = err
-		//	}
-		//}
-	}
-	return errors.Trace(resultError)
-}
-
-//func (cs *ContainerSetup) initialiseAndStartProvisioner(
-//	abort <-chan struct{}, containerType instance.ContainerType,
-//) (resultError error) {
-//	// Flag that this container type has been handled.
-//	atomic.StoreInt32(cs.setupDone[containerType], 1)
-//
-//	defer func() {
-//		if resultError != nil {
-//			cs.logger.Warningf("not stopping machine agent container watcher due to error: %v", resultError)
-//			return
-//		}
-//		if atomic.AddInt32(&cs.numberProvisioners, 1) == int32(len(cs.supportedContainers)) {
-//			// We only care about the initial container creation.
-//			// This worker has done its job so stop it.
-//			// We do not expect there will be an error, and there's not much we can do anyway.
-//			if err := cs.runner.StopAndRemoveWorker(cs.workerName, abort); err != nil {
-//				cs.logger.Warningf("stopping machine agent container watcher: %v", err)
-//			}
-//		}
-//	}()
-//
-//	cs.logger.Debugf("setup and start provisioner for %s containers", containerType)
-//
-//	// Do an early check.
-//	if containerType != instance.LXD && containerType != instance.KVM {
-//		return fmt.Errorf("unknown container type: %v", containerType)
-//	}
-//
-//	// Get the container manager config before other initialisation,
-//	// so we know if there are issues with host machine config.
-//	managerConfig, err := containerManagerConfig(containerType, cs.provisioner)
-//	if err != nil {
-//		return errors.Annotate(err, "generating container manager config")
-//	}
-//	managerConfigWithZones, err := broker.ConfigureAvailabilityZone(managerConfig, cs.machine)
-//	if err != nil {
-//		return errors.Annotate(err, "configuring availability zones")
-//	}
-//
-//	if err := cs.initContainerDependencies(abort, containerType, managerConfig); err != nil {
-//		return errors.Annotate(err, "setting up container dependencies on host machine")
-//	}
-//
-//	instanceBroker, err := broker.New(broker.Config{
-//		Name:          "provisioner",
-//		ContainerType: containerType,
-//		ManagerConfig: managerConfigWithZones,
-//		APICaller:     cs.provisioner,
-//		AgentConfig:   cs.config,
-//		MachineTag:    cs.machine.MachineTag(),
-//		MachineLock:   cs.machineLock,
-//		GetNetConfig:  cs.getNetConfig,
-//	})
-//	if err != nil {
-//		return errors.Annotate(err, "initialising container infrastructure on host machine")
-//	}
-//
-//	toolsFinder := getToolsFinder(cs.provisioner)
-//	return StartProvisioner(
-//		cs.runner,
-//		containerType,
-//		cs.provisioner,
-//		cs.config,
-//		// Container provisioners are always good using the global logging
-//		// context.
-//		loggo.GetLogger("juju.worker.provisioner"),
-//		instanceBroker,
-//		toolsFinder,
-//		getDistributionGroupFinder(cs.provisioner),
-//		cs.credentialAPI,
-//	)
-//}
 
 // initContainerDependencies ensures that the host machine is set-up to manage
 // containers of the input type.
@@ -256,17 +114,19 @@ func (cs *ContainerSetup) observeNetwork() ([]params.NetworkConfig, error) {
 }
 
 func (cs *ContainerSetup) acquireLock(comment string) (func(), error) {
+	// temporary
+	// TODO how to get an abort channel here.
+	timeout := make(chan struct{})
+	go func() {
+		<-time.After(1 * time.Minute)
+		close(timeout)
+	}()
 	spec := machinelock.Spec{
-		Cancel:  cs.abort,
+		Cancel:  timeout,
 		Worker:  "provisioner",
 		Comment: comment,
 	}
 	return cs.machineLock.Acquire(spec)
-}
-
-// TearDown is defined on the StringsWatchHandler interface. NoOp here.
-func (cs *ContainerSetup) TearDown() error {
-	return nil
 }
 
 // getContainerInitialiser exists to patch out in tests.
@@ -289,42 +149,4 @@ func containerManagerConfig(
 	}
 	managerConfig := container.ManagerConfig(managerConfigResult.ManagerConfig)
 	return managerConfig, nil
-}
-
-// Override for testing.
-var StartProvisioner = startProvisionerWorker
-
-// startProvisionerWorker kicks off a provisioner task responsible for creating
-// containers of the specified type on the machine.
-func startProvisionerWorker(
-	runner *worker.Runner,
-	containerType instance.ContainerType,
-	provisioner *apiprovisioner.State,
-	config agent.Config,
-	logger Logger,
-	broker environs.InstanceBroker,
-	toolsFinder ToolsFinder,
-	distributionGroupFinder DistributionGroupFinder,
-	credentialAPI workercommon.CredentialAPI,
-) error {
-
-	workerName := fmt.Sprintf("%s-provisioner", containerType)
-	// The provisioner task is created after a container record has
-	// already been added to the machine. It will see that the
-	// container does not have an instance yet and create one.
-	return runner.StartWorker(workerName, func() (worker.Worker, error) {
-		w, err := NewContainerProvisioner(containerType,
-			provisioner,
-			logger,
-			config,
-			broker,
-			toolsFinder,
-			distributionGroupFinder,
-			credentialAPI,
-		)
-		if err != nil {
-			return nil, errors.Trace(err)
-		}
-		return w, nil
-	})
 }
