0cf880d1527cbddbdf9b298478cb6a1a39250931
Heather Lanigan <heather.lanigan@canonical.com>
2026-01-06 19:52:50 +0000

    refactor: move update network info to CommitHookChanges domain method
    Part of making CommitHook be a single domain call and done as one db
    transaction.

--- apiserver/facades/agent/uniter/uniter.go
+++ apiserver/facades/agent/uniter/uniter.go
@@ -2808,14 +2808,8 @@ func (u *UniterAPI) commitHookChangesForOneUnit(
 		return internalerrors.Errorf("parsing unit name: %w", err)
 	}
 	arg := unitstate.CommitHookChangesArg{
-		UnitName: unitName,
-	}
-
-	if changes.UpdateNetworkInfo {
-		err := u.setUnitRelationNetworks(ctx, unitName)
-		if err != nil {
-			return internalerrors.Errorf("updating network info: %w", err)
-		}
+		UpdateNetworkInfo: changes.UpdateNetworkInfo,
+		UnitName:          unitName,
 	}
 
 	relationUnitSettings := make([]unitstate.RelationSettings, 0, len(changes.RelationUnitSettings))
